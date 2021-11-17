d92be4de8bd491bd58966b334d19b13e32e1ae4c
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-11-17 10:03:46 +0100

    Create 1.1ServerSideProgramming.js

---
+++ 1.1ServerSideProgramming.js
@@ -0,0 +1,37 @@
+/*-------------------------------------*/ SERVER-SIDE PROGRAMMING
+/*-------------------------------------*/
+// this means writing code that runs on another computer functioning as SERVER
+// using DATABASE to store and retrieve information
+// login pages are almost always created with the help of SERVER-SIDE code
+// server-side code helps provide a tailored experience for each user
+// TAILORED EXPERIENCE = dynamic experience
+// server-side code creates LOGIN pages with USERNAME and PASSWORD
+// opening a social network and showing a list of a user's friends
+// => this situation requires a TAILORED EXPERIENCE
+// creating TAILORED EXPERIENCES, like showing a user's msgs after logging in
+// => this is a KEY SKILL in modern full-stack web development
+
+
+/*-------------------------------------*/
+// tailored webpages start with HTML
+// usually when clicking a link, the browser makes a "HTTP request" to retrieve some new data
+// HTTP = HyperText Transfer Protocol
+// "WEB BROWSERS" use HTTP (HyperText Transfer Protocol) to communicate with "WEB SERVERS"
+// it is how the browser communicates with the server when you click on a URL link
+
+/*-------------------------------------*/
+// the browser makes a REQUEST to the server (REQUESTING DATA)
+// the server sends back a RESPONSE (RECEIVING a RESPONSE)
+
+           Request
+__________          __________
+|        |          |        |
+| Browser| =======> | Server |
+|________|          |________|
+
+
+           Response
+__________          ___________
+|        |          |         |
+| Server | <======= | Browser |
+|________|          |_________|
