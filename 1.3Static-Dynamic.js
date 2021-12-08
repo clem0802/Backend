cd27e568b0d20e6e807287ffac58342650d74656
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-12-08 22:26:09 +0100

    Create 1.3Static-Dynamic.js

---
+++ 1.3Static-Dynamic.js
@@ -0,0 +1,15 @@
+-a static webpage serving up hardcoded HTML might be a company "About" page.
+-the information doesn't change, so there's no dynamic content.
+-a static website => a blog's pizza recipe page
+-dynamic websites generate some of their content dynamically
+-by requesting data from the server and displaying the response
+-to make the website dynamic, we could use each product's "DIV" as a template
+-filling it with th eproduct data we request from the BACKEND
+
+-dynamic websites can return "DIFFERENT CONTENT" for a request 
+-based on information in the URL or other information stored in the browser
+-https://catalog.com/api/products?results=10
+-it should return "10 results"
+
+-what do we generally call the combination of using a database and code on the server?
+  -BACKEND or SERVER-SIDE CODING
