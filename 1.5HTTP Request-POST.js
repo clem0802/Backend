83fd5580c529b03968a88335b44509eb018dbe99
clem0802 <88548513+clem0802@users.noreply.github.com>
2022-02-11 15:16:54 +0100

    Create 1.5HTTP Request-POST.js

---
+++ 1.5HTTP Request-POST.js
@@ -0,0 +1,9 @@
+/*------------------------------*/ (POST)
+POST is another common method, which helps us create something new on the backend. 
+For example, adding a new user to the database.
+=> POST https://coolproducts.com/signup/newAcccount
+
+Examples of a POST request include creating a new account, 
+writing a new blog post entry, or uploading a new social media post.
+=> POST https://insta.com/posts/new
+=> POST https://obscurewriters.com/writers
