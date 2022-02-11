bd37a76ab51a1b1bf279c6351363e24ed7c2be7d
clem0802 <88548513+clem0802@users.noreply.github.com>
2022-02-11 15:16:57 +0100

    Create 1.6HTTP Request-DELETE.js

---
+++ 1.6HTTP Request-DELETE.js
@@ -0,0 +1,5 @@
+/*------------------------------*/ (DELETE)
+DELETE is another common HTTP method that removes something on the server, 
+like deleting an account or a post.
+=> DELETE https://coolproducts.com/robots/newrobot/6
+=> DELETE https://obscurewriters.com/writers/1034
