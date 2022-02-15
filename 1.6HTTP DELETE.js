92df40e9a6f85946da65ec9409b9cc559795256d
clem0802 <88548513+clem0802@users.noreply.github.com>
2022-02-15 21:02:59 +0100

    Update 1.6HTTP DELETE.js

--- 1.6HTTP DELETE.js
+++ 1.6HTTP DELETE.js
@@ -3,3 +3,7 @@ DELETE is another common HTTP method that removes something on the server,
 like deleting an account or a post.
 => DELETE https://coolproducts.com/robots/newrobot/6
 => DELETE https://obscurewriters.com/writers/1034
+
+/*------------------------------*/ (DELETE)
+Remove the user with the ID "123" from "onlinegames.com
+DELETE https://onlinegames.com/users/123
